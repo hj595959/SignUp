ac0aad209be071356861445e08c2bbd5e0d124c2
hj595959 <favor3737@gmail.com>
2022-10-04 17:31:50 +0900

    이효정

--- app/src/main/java/com/example/signup/MainActivity.java
+++ app/src/main/java/com/example/signup/MainActivity.java
@@ -2,27 +2,49 @@ package com.example.signup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton nextBtn;
+    private Button pwcheck;
+    private EditText pw, pw2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nextBtn=findViewById(R.id.nextBtn);
-        nextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, basicLogin.class);
-                startActivity(intent); //액비 이동
+
+        //뒤로 가기 버튼
+        Button back = findViewById(R.id.back);
+        back.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, account.class);
+            startActivity(intent);
+        });
+
+
+
+        //비밀번호 확인 버튼
+        pwcheck = findViewById(R.id.pwcheckbutton);
+        pwcheck.setOnClickListener(v -> {
+            if(pw.getText().toString().equals(pw2.getText().toString())){
+                pwcheck.setText("일치");
+            }else{
+                Toast.makeText(this,"비밀번호가 다릅니다.", Toast.LENGTH_LONG).show();
             }
         });
+
     }
-}
+
+
+
+
+
+    }
