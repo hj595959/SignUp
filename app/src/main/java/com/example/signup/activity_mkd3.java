10b4df7f79811b422efa2715ac39f878e5c622b4
sehong <wrsf8989@daum.net>
2022-10-08 11:25:59 +0900

    sehongCommit

--- app/src/main/java/com/example/signup/activity_mkd3.java
+++ app/src/main/java/com/example/signup/activity_mkd3.java
@@ -1,19 +1,26 @@
 package com.example.signup;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class activity_mkd3 extends AppCompatActivity {
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private DatabaseReference databaseReference = database.getReference();
+    private DatabaseReference databaseReference = database.getReference("member");
     //입력값 받아오기
     private EditText signPW , signmail;
     private Button signUpBtn;
@@ -30,6 +37,36 @@ public class activity_mkd3 extends AppCompatActivity {
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final ProgressDialog dislog = new ProgressDialog(activity_mkd3.this);
+                String str_signmail = signmail.getText().toString();
+                String str_signPW = signPW.getText().toString();
+                Query memberQuery = databaseReference.child("member").orderByChild("signmail").equalTo(str_signmail);
+                memberQuery.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
             }
         });
