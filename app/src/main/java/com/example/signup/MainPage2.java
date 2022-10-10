366d825e50493d4de993afc0002f4b20a203db9e
hj595959 <favor3737@gmail.com>
2022-10-10 19:43:05 +0900

    이효정

--- app/src/main/java/com/example/signup/MainPage2.java
+++ app/src/main/java/com/example/signup/MainPage2.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Mainpage2 extends AppCompatActivity {
+public class MainPage2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
