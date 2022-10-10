4da8ddc7a6c8b48d965bcb2b54f4332f8894de6c
hj595959 <favor3737@gmail.com>
2022-10-10 21:22:48 +0900

    이효정

--- app/src/main/java/com/example/signup/Mypage.java
+++ app/src/main/java/com/example/signup/Mypage.java
@@ -1,8 +1,12 @@
 package com.example.signup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class Mypage extends AppCompatActivity {
 
@@ -11,6 +15,12 @@ public class Mypage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        Button mypage = findViewById(R.id.mypage);
+        mypage.setOnClickListener(v -> {
+            Intent intent = new Intent(Mypage.this, user_info.class);
+            startActivity(intent);
+        });
 
 
 
