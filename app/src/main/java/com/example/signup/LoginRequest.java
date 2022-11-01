73f445f92c600c9550052845aa66a2f56c0d6199
hj595959 <favor3737@gmail.com>
2022-11-01 21:03:04 +0900

    이효정

--- app/src/main/java/com/example/signup/LoginRequest.java
+++ app/src/main/java/com/example/signup/LoginRequest.java
@@ -1,6 +1,5 @@
 package com.example.signup;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 
@@ -22,7 +21,7 @@ public class LoginRequest extends StringRequest {
     }
 
     @Override
-    protected Map<String, String> getParams() throws AuthFailureError {
+    protected Map<String, String> getParams(){
         return map;
     }
 }
--- app/src/main/java/com/example/signup/activity_mkd3.java
+++ app/src/main/java/com/example/signup/activity_mkd3.java
@@ -13,6 +13,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class activity_mkd3 extends AppCompatActivity {
@@ -29,18 +30,20 @@ public class activity_mkd3 extends AppCompatActivity {
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String userID = signID.getText().toString();
-                final String userPassword = signPW.getText().toString();
+                String userID = signID.getText().toString();
+                String userPassword = signPW.getText().toString();
+
 
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+
                         try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
                             if (success) {
-                                String userID = jsonResponse.getString("userID");
-                                String userPassword = jsonResponse.getString("userPassword");
+                                String userID = jsonObject.getString("userID");
+                                String userPassword = jsonObject.getString("userPassword");
 
                                 Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(activity_mkd3.this, MainPage1.class);
@@ -52,7 +55,7 @@ public class activity_mkd3 extends AppCompatActivity {
                                 return;
                             }
 
-                        } catch (Exception e) {
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
