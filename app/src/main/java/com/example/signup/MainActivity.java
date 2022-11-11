9705273e36aab04eedd315510814ea460c67db3b
hj595959 <favor3737@gmail.com>
2022-11-11 20:44:50 +0900

    이효정

--- app/src/main/java/com/example/signup/MainActivity.java
+++ app/src/main/java/com/example/signup/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.signup;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -20,21 +23,29 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button signupbutton, signEmailCheck, signIDCheck;
     private String gender_result, serviceId_result;
     private AlertDialog.Builder dialog;
     private boolean validate;
+    private Button signImageBtn;
+    private ImageView imageView;
+    private static final int REQUEST_CODE = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         signupbutton = findViewById(R.id.signupbutton);
-        signEmailCheck =findViewById(R.id.signEmailCheck);
+        signEmailCheck = findViewById(R.id.signEmailCheck);
         signIDCheck = findViewById(R.id.signIDCheck);
+        signImageBtn = findViewById(R.id.signImageBtn);
 
 
         final EditText signName = findViewById(R.id.signName);
@@ -52,6 +63,32 @@ public class MainActivity extends AppCompatActivity {
         final RadioButton caregiver = findViewById(R.id.caregiver); //간병인
         final EditText phoneNb = findViewById(R.id.phoneNb);//i
         final EditText signAddress = findViewById(R.id.signAddress);
+
+        imageView = findViewById(R.id.signImage);
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         //성별 라디오 버튼 상태 값 담기
@@ -121,6 +158,8 @@ public class MainActivity extends AppCompatActivity {
                     String userAddress= signAddress.getText().toString();
 
 
+
+
                     Response.Listener<String> responseListener = new Response.Listener<String>() {
                         @Override
                         public void onResponse(String response) {
@@ -164,38 +203,58 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
                             boolean success = jsonResponse.getBoolean("success");
-                            if(success) {
+                            if (success) {
                                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                                 dialog = builder.setMessage("사용할 수 있는 아이디 입니다.")
-                                       .setPositiveButton("확인",null);
+                                        .setPositiveButton("확인", null);
                                 dialog.show();
                                 signID.setEnabled(false);
                                 validate = true;
                                 signID.setBackgroundColor(getResources().getColor(R.color.colorGrqy));
-                            }
-                            else {
+                            } else {
                                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                                 dialog = builder.setMessage("사용할 수 없는 아이디 입니다.")
-                                        .setNegativeButton("확인",null);
+                                        .setNegativeButton("확인", null);
 
                                 dialog.show();
                             }
-                        }
-                        catch (JSONException ex) {
+                        } catch (JSONException ex) {
                             ex.printStackTrace();
                         }
                     }
 
                 };
-            IDCheckRequest iDCheckRequest = new IDCheckRequest(userID, responseListener);
-            RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-            queue.add(iDCheckRequest);
-        }
-
-
+                IDCheckRequest iDCheckRequest = new IDCheckRequest(userID, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                queue.add(iDCheckRequest);
+            }
         });
 
 
+
+
+
+
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+
+                    imageView.setImageBitmap(img);
+                } catch (Exception e) {
+                }
+            }
+        }
     }}
 
 
